8d7e87accc44ec414bbc8da6d0924987cdd0d756
Øyvind Mathisen <OyvindMathisenWork@gmail.com>
2019-10-24 14:05:43 +0200

    Fix: Fixed errorous express path

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/vipps-case-payex'));
+app.use(express.static(__dirname + '/dist/VippsCaseFrontEnd'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/vipps-case-payex/index.html'));
+res.sendFile(path.join(__dirname+'/dist/VippsCaseFrontEnd/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
